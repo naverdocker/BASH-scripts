e450e9b574840dc2a68d2d23f91602d97fbf8d55
revan <naverdocker@gmail.com>
2022-12-15 12:30:06 +0530

    added exit command inside if condition at the end

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -15,27 +15,29 @@ then
 	sudo apt install fontconfig openjdk-11-jre -y
 	sudo apt install jenkins -y
 	echo "jenkins is installed and available"
-
-else 
-	sudo yum update -y
-
-	if [ $? -eq 0 ]
-	then
-		sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    		https://pkg.jenkins.io/redhat-stable/jenkins.repo
-		sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-		sudo yum upgrade
-		sudo amazon-linux-extras install java-openjdk11 -y
-		sudo yum install jenkins -y
-		sudo systemctl enable jenkins
-		sudo systemctl start jenkins
-		echo "jenkins is installed and available"
-	else
-		echo " "
-		echo " this file can only install jenkins on ubuntu or debain or amazonlinux."
-		echo " "
-		echo " if your distro is one of ubuntu, debian or amazonlinux, then this file is most likely outdated and needs update before execution"
-		echo " "
-		echo " if you distro is neither ubuntu nor debian nor amazonlinux, then visit jenkins.io for info on how to install jenkins on your distro"
-	fi
+	exit
 fi
+
+sudo yum update -y
+
+if [ $? -eq 0 ]
+then
+	sudo wget -O /etc/yum.repos.d/jenkins.repo \
+	https://pkg.jenkins.io/redhat-stable/jenkins.repo
+	sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+	sudo yum upgrade
+	sudo amazon-linux-extras install java-openjdk11 -y
+	sudo yum install jenkins -y
+	sudo systemctl enable jenkins
+	sudo systemctl start jenkins
+	echo "jenkins is installed and available"
+	exit
+else
+	echo " "
+	echo " this file can only install jenkins on ubuntu or debain or amazonlinux."
+	echo " "
+	echo " if your distro is one of ubuntu, debian or amazonlinux, then this file is most likely outdated and needs update before execution"
+	echo " "
+	echo " if you distro is neither ubuntu nor debian nor amazonlinux, then visit jenkins.io for info on how to install jenkins on your distro"
+fi
+
